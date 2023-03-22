8c5a5a1c8fc8d8241031f2cbe364b045053300d3
candraagungpurnomo <122202002820@mhs.dinus.ac.id>
2023-03-22 11:17:36 +0700

    website react-js mini bootcamp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import GambarMobil from './img/mobil.png'
 import './App.css';
+
+// const Welcome = (props) => {
+//   console.log(props.name)
+//   return <div>HEllo lagi {props.name}</div>
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App" style={{color: "red"}}>
+    //   <h1>Hello World</h1>
+    // </div>
+    <div>
+      <div class="box1">
+      <h1>Hi, Nama Saya Candra</h1>
+      <p>Senang Bisa mengenal mu!</p>
+    </div>
+    <div class="box2">
+      <p>Dibawah ini adalah gambar mobil</p>
+    </div>
+    <div class="box3">
+      <img alt="mobil" src={GambarMobil} class="foto" />
+    </div>
+    <div class="box4">
+      <div class="con1"></div>
+      <div class="con2"></div>
+      <div class="con3"></div>
+      <div class="con4"></div>
+    </div>
     </div>
   );
 }
